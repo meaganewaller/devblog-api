bf2da0c6dc5d933e942b6d75a382f19a4069d00d
Meagan Waller <meagan.waller@testdouble.com>
2023-09-13 10:34:50 -0400

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@
 #
 
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :posts, only: %i[index show]
       resources :categories, only: %i[index show]
