fcf06c80f0c773c272b13c714a0efee46cd34e99
Meagan Waller <meagan.waller@testdouble.com>
2023-10-12 09:42:19 -0400

    Adds recent params to posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,12 +4,18 @@ module Api
   module V1
     class PostsController < ApiController
       def index
-        @posts = Post.includes([:category]).published
+        if params[:recent]
+          @posts = Post.includes([:category]).published.limit(5)
+        else
+          @posts = Post.includes([:category]).published
+        end
         @posts = @posts.filter_by_category(params[:category]) if params[:category].present?
         @posts = @posts.filter_by_tag(params[:tag]) if params[:tag].present?
         @posts = @posts.search_post(params[:query]) if params[:query].present?
 
-        @pagy, @posts = pagy(@posts, items: 10)
+        items = params[:limit] || params[:recent] ? 5 : 10
+
+        @pagy, @posts = pagy(@posts, items:)
         @pagy_metadata = pagy_metadata(@pagy)
       end
 
