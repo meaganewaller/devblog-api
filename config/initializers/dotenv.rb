d8b5b7c405e8af9d93b1e3de4e3c27a0ffc78c54
Meagan Waller <meagan@meaganwaller.com>
2024-03-19 15:36:55 -0400

    Add dotenv guard to ensure it only runs in dev and test

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
-Dotenv.require_keys('PORT', 'CLIENT_ORIGIN_URL', 'NOTION_BLOG_DATABASE_ID', 'NOTION_CATEGORY_DATABASE_ID',
-                    'NOTION_CONTACT_FORM_DATABASE_ID', 'NOTION_TOKEN')
+if Rails.env.development? || Rails.env.test?
+  require 'dotenv'
+  Dotenv.load('.env')
+  Dotenv.require_keys('PORT', 'CLIENT_ORIGIN_URL', 'NOTION_BLOG_DATABASE_ID', 'NOTION_CATEGORY_DATABASE_ID',
+                      'NOTION_CONTACT_FORM_DATABASE_ID', 'NOTION_TOKEN')
+end
